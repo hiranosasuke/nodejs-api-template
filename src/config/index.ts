a41cbc9f0ab97fd448b614a2010b128672f2628f
hiranosasuke <hiranosasuke.com>
2024-05-16 22:28:22 -0400

    Added body-parser

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,18 @@
 import { Router, Express } from 'express';
 
+type Wrapper = (router: Router) => void;
+
+export const applyMiddleware = (
+  middlewareWrappers: Wrapper[],
+  router: Router
+) => {
+  for (const wrapper of middlewareWrappers) {
+    wrapper(router);
+  }
+};
+
 export const applyRoutes = (routes: Router[], app: Express) => {
-	for (const route of routes) {
-		app.use('/api/', route);
-	}
+  for (const route of routes) {
+    app.use('/api/', route);
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 require('dotenv').config();
 import express, { Request, Response } from 'express';
-import { applyRoutes } from './config/index';
+import { applyMiddleware, applyRoutes } from './config/index';
 import { applySwagger } from './config/swagger.config';
 import routes from './api/routes/index';
+import middleware from './api/middlewares';
 
 const app = express();
 
+applyMiddleware(middleware, app);
 applyRoutes(routes, app);
 applySwagger(app);
 
 app.listen(3000, () => {
-	console.log('Server running on http://localhost:3000');
+  console.log('Server running on http://localhost:3000');
 });
